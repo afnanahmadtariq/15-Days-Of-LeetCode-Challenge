8402cc2316265884d80b8f0583186ff265499b63
ProgramerG <116060169+afnanahmadtariq@users.noreply.github.com>
2024-11-27 18:15:08 +0500

    Time: 419 ms (93.91%), Space: 20.4 MB (43.03%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,36 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        triples = set()
+        n, p, z = [], [], []
+        for num in nums:
+            if num > 0:
+                p.append(num)
+            elif num < 0: 
+                n.append(num)
+            else:
+                z.append(num)
+
+        N, P = set(n), set(p)
+
+        if z:
+            for num in P:
+                if -1*num in N:
+                    triples.add((-1*num, 0, num))
+
+        if len(z) >= 3:
+            triples.add((0,0,0))
+
+        for i in range(len(n)):
+            for j in range(i+1,len(n)):
+                target = -1*(n[i]+n[j])
+                if target in P:
+                    triples.add(tuple(sorted([n[i],n[j],target])))
+        for i in range(len(p)):
+            for j in range(i+1,len(p)):
+                target = -1*(p[i]+p[j])
+                if target in N:
+                    triples.add(tuple(sorted([p[i],p[j],target])))
+
+        return list(triples)
+        
