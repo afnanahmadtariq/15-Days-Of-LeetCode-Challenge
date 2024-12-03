aafe066333ca243c1ad78253f50820d324427d6e
Afnan Ahmad Tariq <116060169+afnanahmadtariq@users.noreply.github.com>
2024-12-03 20:39:11 +0500

    Time: 3 ms (82.74%), Space: 18.9 MB (5.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count = collections.Counter()
-        for num in nums:
-            count[num] += 1
-            if count[num] > len(nums)/2:
-                return num
+        count.update(nums)
+        num = max(count.values())
+        for key, value in count.items():
+            if value == num:
+                return key
+
         
