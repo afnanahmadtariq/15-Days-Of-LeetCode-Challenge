a4c1e1232aa281dcdb1559ecc20b732f0880327b
Afnan Ahmad Tariq <116060169+afnanahmadtariq@users.noreply.github.com>
2024-12-09 05:06:09 +0500

    Time: 3 ms (98.62%), Space: 18.1 MB (11.44%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -3,21 +3,15 @@ class Solution:
         n = len(nums)
         if n == 1:
             return nums[0]
-        
         max_value = nums[0]
         left_value = right_value = 1
-        
         for i in range(n):
-            # Left to right pass
             left_value *= nums[i]
             max_value = max(max_value, left_value)
             if left_value == 0:
-                left_value = 1  # Reset if zero is encountered
-            
-            # Right to left pass
+                left_value = 1
             right_value *= nums[n - i - 1]
             max_value = max(max_value, right_value)
             if right_value == 0:
-                right_value = 1  # Reset if zero is encountered
-
+                right_value = 1
         return max_value
